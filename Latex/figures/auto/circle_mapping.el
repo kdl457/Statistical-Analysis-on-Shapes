98afba8136b16db4e1c7049b2fb089bf85734a08
Anders Munch <amnudn@gmail.com>
2018-10-15 23:24:36 +0200

    rough draft of the construction of the manifolds of curves

--- Latex/figures/auto/circle_mapping.el
+++ Latex/figures/auto/circle_mapping.el
@@ -1,6 +1,8 @@
 (TeX-add-style-hook
  "circle_mapping"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("standalone" "border={0pt -30pt 0pt -15pt}")))
    (TeX-run-style-hooks
     "latex2e"
     "standalone"
--- Latex/figures/auto/circle_vectorfield.el
+++ Latex/figures/auto/circle_vectorfield.el
@@ -1,6 +1,8 @@
 (TeX-add-style-hook
  "circle_vectorfield"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("standalone" "border={-30pt -10pt -30pt -10pt}")))
    (TeX-run-style-hooks
     "latex2e"
     "standalone"
